81aa09581b6e496d0c170c13a527549278158111
Nicholas Paro <paro.nicholas@gmail.com>
2019-09-10 11:56:34 -0700

    day1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Task Master
+
+## Table of Contents
+* [Description](#description)
+* [Link](#link)
+* [Issues](#issues)
+
+### Description <a name="description"></a>
+
+
+### Link <a name="link"></a>
+
+### Issues <a name="issues"></a>
---
+++ src/main/java/com/nparo/TaskMaster/repository/TasksRepository.java
@@ -0,0 +1,12 @@
+package com.nparo.TaskMaster.repository;
+
+import com.nparo.TaskMaster.models.Tasks;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface TasksRepository extends CrudRepository<Tasks, String> {
+  Optional<Tasks> findById(String id);
+}
